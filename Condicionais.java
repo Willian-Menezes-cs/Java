cda66e67ffb07776d6cec40f1d8dbc848215e383
Willian-Menezes-cs <willian.menezes01@outlook.com>
2024-08-30 21:31:43 -0300

    Add files via upload

---
+++ Condicionais.java
@@ -0,0 +1,121 @@
+
+package condicionais;
+  import java.util.Scanner;
+
+public class Condicionais {
+
+  
+    public static void main(String[] args) {
+        
+        Scanner scann = new Scanner(System.in);
+        
+        
+         //System.out.println("escreva sua idade");
+        
+        //int idade = scann.nextInt();
+
+       // if (idade >=18) {
+            //System.out.println("maior de idade");
+       // } else {
+           // System.out.println("menor de idade");
+           
+           
+           
+           
+           
+           
+        
+//           System.out.println("verifique um triangulo");
+//           
+//           System.out.println("escreva um numero");
+//             int n1 = scann.nextInt();
+//             
+//           System.out.println("escreva um numero");
+//             int n2 = scann.nextInt();
+//             
+//           System.out.println("escreva um numero");
+//             int n3 = scann.nextInt();
+//           
+//           if (n1+n2>n3) {
+//            System.out.println("é um triangulo");
+//           } else {
+//               System.out.println("não é triangulo"); 
+//           }
+//             if (n1+n2>n3) {
+//               System.out.println("é um triangulo");
+//           } else {
+//               System.out.println("não é triangulo"); 
+//           }
+//           
+           
+           
+           
+//           System.out.println("descubra a media");
+//           
+//           System.out.println("escreva a primeria nota");
+//           double x1 = scann.nextDouble();
+//           
+//           System.out.println("escreva a segunda nota");
+//           double x2 = scann.nextDouble();
+//           
+//           System.out.println("escreva a terceira nota");
+//           double x3 = scann.nextDouble();
+//           
+//           double nota_final = x1+x2+x3 /3;
+//           
+//              if (nota_final >=6) {
+//            System.out.println("aprovado");
+//            } else {
+//               System.out.println("reprovado");
+//              }
+
+        System.out.println("Descubra o salario final");
+        
+        System.out.println("Salario inicial");
+        double salario = scann.nextDouble();
+        
+        double salario_aumentado = salario + (salario * 0.15);
+        
+        double salario_final = salario_aumentado -(salario_aumentado * 0.08);
+        
+        System.out.println("o salario inicial apos aumento de 15% e o desconto de imposto é:" + salario_final);
+        
+        
+        
+        
+            
+           
+           
+                  
+           
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    }
+    
+
